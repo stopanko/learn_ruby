823eef49c88db3919f5f0969e69eb5b476cf094d
stopanko <stepander007@gmail.com>
2015-12-12 22:34:53 +0200

    02 - pass bonus examples

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -11,3 +11,28 @@ def sum(array)
 	array.each{|i| sum += i}
 	sum
 end	
+
+# def multiply(first, second)
+# 	res = 1
+# 	res = first * second
+# end	
+
+def multiply(*array)
+	res = 1
+	if array.size > 1
+		array.each{|i| res *= i}
+	else
+		array[0].each{|i| res *= i}
+	end
+	res
+end
+
+def power(number,power)
+	number**power || 0
+end	
+
+def factorial(n)
+	fact = 1
+	(1..n).step(1){|i| fact *= i}
+	fact
+end	
